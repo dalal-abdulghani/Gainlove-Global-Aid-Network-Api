6c6f334def5f8b3f7e85e176453913b2abe152f4
dalal-abdulghani <dalal.m.abdulghani@gmail.com>
2025-08-24 12:10:28 +0300

    chore: update composer autoload and config settings

--- config/config.php
+++ config/config.php
@@ -1,14 +1,9 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-define("DB_HOST", $_ENV['DB_HOST']);
-define("DB_NAME", $_ENV['DB_NAME']);
-define("DB_USER", $_ENV['DB_USER']);
-define("DB_PASS", $_ENV['DB_PASS']);
-define("JWT_SECRET", $_ENV['JWT_SECRET']);
-define("APP_ENV", $_ENV['APP_ENV']);
-define("APP_DEBUG", $_ENV['APP_DEBUG']);
-define("APP_URL", $_ENV['APP_URL']);
+define("DB_HOST", "localhost");
+define("DB_NAME", "GainLove_api");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("JWT_SECRET", "mySuperSecretKey");
+define("APP_ENV", "development");
+define("APP_DEBUG", true);
+define("APP_URL", "http://localhost");
